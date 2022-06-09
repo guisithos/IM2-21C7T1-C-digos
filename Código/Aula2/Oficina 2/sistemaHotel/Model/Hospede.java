eac25edf3dcd4a3160d983d354c1dd0fcdec557a
Guilherme Thomas <gui.thomas@outlook.com>
2022-06-09 15:32:01 -0300

    Create Hospede.java

---
+++ Código/Aula2/Oficina 2/sistemaHotel/Model/Hospede.java
@@ -0,0 +1,66 @@
+package br.dell.model;
+
+public class Hospede {
+    private String nome;
+    private String endereco;
+    private int idade;
+    private long cpf;
+    private long rg;
+    private int numQuarto;
+
+    public Hospede(String nome, String endereco, int idade, long cpf, long rg, int numQuarto) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.idade = idade;
+        this.cpf = cpf;
+        this.rg = rg;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public long getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(long cpf) {
+        this.cpf = cpf;
+    }
+
+    public long getRg() {
+        return rg;
+    }
+
+    public void setRg(long rg) {
+        this.rg = rg;
+    }
+
+    public int getNumQuarto() {
+        return numQuarto;
+    }
+
+    public void setNumQuarto(int numQuarto) {
+        this.numQuarto = numQuarto;
+    }
+}
