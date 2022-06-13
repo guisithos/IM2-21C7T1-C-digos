39a400300d9805ea485bf40ce3d65a0e492346b3
Guilherme Thomas <gui.thomas@outlook.com>
2022-06-13 10:23:37 -0300

    Update Principal.java

--- Código/Aula3/Oficina3/sistemaHotel/Principal.java
+++ Código/Aula3/Oficina3/sistemaHotel/Principal.java
@@ -1,1 +1,27 @@
 
+import br.dell.model.EntradaSaida;
+import br.dell.model.Sistema;
+
+public class Principal
+{
+    public static void main(String s[]) 
+    {
+        String res = "sim";
+        int opt = 0;
+        while (res.equalsIgnoreCase("sim")) 
+        {
+            opt = EntradaSaida.receberInt("1: cad, 2: pesq");
+            switch(opt) {
+                case 1:
+                    Sistema.cadastrarHospede();
+                    break;
+                case 2:
+                    Sistema.pesquisarHospede();
+                    break;
+                default:
+                    EntradaSaida.mostrarTexto("Inválida");
+            }
+            res = EntradaSaida.receberString("Continuar?");
+        }
+    }
+}
