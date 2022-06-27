195326a6245c61fe565df93effc6aee0929ef2a1
Guilherme Thomas <gui.thomas@outlook.com>
2022-06-27 08:52:04 -0300

    javadoc

---
+++ Código/Aula4/Oficina4/Principal.java
@@ -0,0 +1,32 @@
+
+import br.dell.model.EntradaSaida;
+import br.dell.model.Sistema;
+
+/**
+ * @author: Guilherme Thomas
+ * @version: v0.5
+ */
+
+public class Principal
+{
+    public static void main(String s[]) 
+    {
+        String res = "sim";
+        int opt = 0;
+        while (res.equalsIgnoreCase("sim")) 
+        {
+            opt = EntradaSaida.receberInt("1: cadastro, 2: pesq");
+            switch(opt) {
+                case 1:
+                    Sistema.cadastrarHospede();
+                    break;
+                case 2:
+                    Sistema.pesquisarHospede();
+                    break;
+                default:
+                    EntradaSaida.mostrarTexto("Inválida");
+            }
+            res = EntradaSaida.receberString("Continuar?");
+        }
+    }
+}
